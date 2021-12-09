8c1204b1817136c012464d3ba0eede29dcb68f4f
Thamar <thamar-duikelaar@outlook.com>
2021-12-10 00:13:37 +0100

    Update to support DSwap
    Update to support DSwap
    And some dead links updated

--- dposconfig.py
+++ dposconfig.py
@@ -7,6 +7,7 @@ ark_type_nodes = {
     'QreditV2',
     'Blockpool',
     'Hydra',
+    'DSwap',
 }
 
 lisk_type_nodes = {
@@ -27,13 +28,14 @@ otherdpos_type_nodes = {
 
 # Network Settings [#delegates / blocktime in sec. / explorer URL / to reach wallet / node URL needed for API-calls]
 networksettings = {
-    'Ark': [51,         8, 'https://explorer.ark.io',           '/address/',    'http://213.32.9.96:4003/api'],
+    'Ark': [51,         8, 'https://explorer.ark.io',           '/wallets/',    'http://213.32.9.96:4003/api'],
     'DArk': [51,        8, 'https://dexplorer.ark.io',          '/wallets/',    'http://52.191.185.7:4003/api'],       # thamar's node - please use your own relay node
     'Bind main': [47,   6, 'https://bindscan.io',               '/address/',    'http://46.101.255.171:4003/api'],
     'Bind dev': [47,    6, 'https://testnet.bindscan.io',       '/address/',    'http://104.209.180.20:7003/api'],     # thamar's node - please use your own relay node
     'QreditV2': [51,    8, 'https://explorer.sh',               '/qredit/wallet/',    'http://52.191.185.7:5103/api'],
     'Blockpool': [201, 15, 'http://bplexp.blockpool.io',        '/address/',    'http://api.mainnet.blockpool.io:9031/api'],
     'Hydra': [53,       8, 'http://hydra.iop.global/#',         '/address/',    'http://35.195.150.223:4703/api/v2'],
+    'DSwap': [51,       8, 'http://explorer.swipechain.org',   '/#/wallets/',    'http://13.92.194.196:4003/api'],     # thamar's node - please use your own relay node
 
     # Lisk based blockchains
     'LeaseHold': [39,   10, 'https://explorer.leasehold.io',    '/address/',    'http://52.252.112.187:8010'],      # thamar's node - please use your own relay node
@@ -41,13 +43,14 @@ networksettings = {
     'Lisk test': [101,  10, 'https://testnet.liskscan.com',     '/account/',    'https://testnet-service.lisktools.eu/api/v2'],
 
     # Other DPoS blockchains
-    'Adamant': [101,     5, 'https://explorer.adamant.im',      '/address/',    'https://bid.adamant.im/api'],
-    'Rise': [250,       30, 'https://explorer.rise.vision',     '/address/',    'http://51.158.164.24:5555/api'],
+    'Adamant': [101,     5, 'https://explorer.adamant.im',      '/address/',    'https://endless.adamant.im/api'], # was https://bid.adamant.im/api
+    'Rise': [250,       30, 'https://explorer.rise.vision',     '/address/',    'http://52.151.53.25:5555/api'],
 
     # Special DPoS blockchains - GNY
     'GNY test': [101,   10, 'https://testnet.explorer.gny.io',  '/account-detail?address=',    'https://testnet.gny.io'],
     'GNY main': [101,   10, 'https://explorer.gny.io',          '/account-detail?address=',    'https://mainnet.gny.io'],
 }
+
 
 # #####################################################################################################################
 # Old not working or depricated chains/entry's
